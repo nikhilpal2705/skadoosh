725b6e089dc973f63952509bce3313bb28ea7504
Nikhil Suryavanshi <nikhilpal2705@gmail.com>
2017-07-11 00:24:50 +0530

    AOSiP ROM

--- compress.bash
+++ compress.bash
@@ -8,15 +8,15 @@
 ### Manifest Configuration ###
 # Name of the ROM. No Spaces Please.
 # Example: CyanogenMod
-name=GZR-ValidusOS
+name=AOSiP
 
 # Manifest link. https:// is mandatory.
 # Example: https://github.com/cyanogenmod/android
-manifest=https://github.com/GZR-ValidusOS/manifest
+manifest=https://github.com/AOSIP/platform_manifest
 
 # Manifest branch.
 # Example: cm-14.0
-branch=n7.1.2
+branch=nougat-mr2
 
 ### Finally, execute the stuff. ###
 /bin/bash skadoo.sh $name $manifest $branch
